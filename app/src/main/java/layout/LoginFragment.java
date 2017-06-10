2a7c0ddc464b9b464e82ca874746ba6d5cab7b7e
w53387 <w53387@student.wsiz.rzeszow.pl>
2017-06-10 17:58:08 +0200

    perform user input validation in client

--- app/src/main/java/layout/LoginFragment.java
+++ app/src/main/java/layout/LoginFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.wsiiz.footballclient.R;
 
@@ -28,6 +29,32 @@ public class LoginFragment extends Fragment {
         etEmail = (EditText) view.findViewById(R.id.etEmail);
         etPassword = (EditText) view.findViewById(R.id.etPassword);
         btnConfirm = (Button) view.findViewById(R.id.btnLoginSign);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( etEmail.getText().toString().length() == 0 ){
+                    etEmail.setError( "E-mail is required!" );
+                }
+                if (!etEmail.getText().toString().contains("@")) {
+                    etEmail.setError( "It's not an E-mail" );
+                }
+                if (etPassword.getText().toString().length() == 0 ) {
+                    etPassword.setError("Type password");
+                }
+                if (etEmail.getError() == null && etPassword.getError() == null) {
+                    Log.i("Error","OK");
+                    //perform login logic
+                } else {
+                    Toast.makeText(getActivity(),"Fix all input errors",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
+
         return view;
     }
 
--- app/src/main/java/layout/RegisterFragment.java
+++ app/src/main/java/layout/RegisterFragment.java
@@ -7,15 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.wsiiz.footballclient.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RegisterFragment extends Fragment {
 
     private EditText etName, etEmail, etTeam, etPasswd, etConfirmPasswd;
     private Button btnReg;
+    boolean validResult = true;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,10 +32,61 @@ public class RegisterFragment extends Fragment {
         etName = (EditText) view.findViewById(R.id.etName);
         etEmail = (EditText) view.findViewById(R.id.etMailReg);
         etTeam = (EditText) view.findViewById(R.id.etFavorTeam);
-        etName = (EditText) view.findViewById(R.id.etPasswdReg);
-        etName = (EditText) view.findViewById(R.id.etRepeatReg);
+        etPasswd = (EditText) view.findViewById(R.id.etPasswdReg);
+        etConfirmPasswd = (EditText) view.findViewById(R.id.etRepeatReg);
         btnReg = (Button) view.findViewById(R.id.btnReg);
+
+        final List<EditText> views = new ArrayList<>();
+        views.addAll(Arrays.asList(etConfirmPasswd,etEmail,etName,etPasswd,etTeam));
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                getActivity().getWindow().setSoftInputMode(
+                        WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+                );
+                //performing user input validation
+                if (checkIfEmpty(etName)) {
+                    etName.setError("Required field");
+                }
+                if (checkIfEmpty(etEmail)) {
+                    etEmail.setError("Required field");
+                }
+                if (!etEmail.getText().toString().contains("@")) {
+                    etEmail.setError("Not valid email");
+                }
+                if (checkIfEmpty(etPasswd)) {
+                    etPasswd.setError("Required field");
+                }
+                if (checkIfEmpty(etConfirmPasswd)) {
+                    etConfirmPasswd.setError("Required field");
+                }
+                if (!etPasswd.getText().toString().equals(etConfirmPasswd.getText().toString())) {
+                    etConfirmPasswd.setError("Password's do not match");
+                }
+                for (EditText et : views) {
+                   if (et.getError() == null) {
+                       validResult = false;
+                       break;
+                   }
+                }
+                if (validResult == true) {
+                    //perform registr logic
+                } else {
+                    Toast.makeText(getActivity(),"Fix all fields to continue",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return view;
+    }
+
+    public boolean checkIfEmpty (EditText editTextField) {
+        if (editTextField.getText().toString().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
