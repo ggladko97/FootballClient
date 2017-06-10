052ac65dc85c0557a1b29ee84e573a61f65f2f74
w53387 <w53387@student.wsiz.rzeszow.pl>
2017-06-10 16:31:22 +0200

    Initial commit

---
+++ app/src/main/java/com/wsiiz/footballclient/MainActivity.java
@@ -0,0 +1,32 @@
+package com.wsiiz.footballclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin, btnRegister;
+
+    private FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        container = (FrameLayout) findViewById(R.id.containerMain);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+    }
+}
---
+++ app/src/main/java/layout/RegisterFragment.java
@@ -0,0 +1,32 @@
+package layout;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wsiiz.footballclient.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RegisterFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RegisterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+
+}
